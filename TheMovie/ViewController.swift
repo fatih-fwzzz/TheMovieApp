d68f1d1fadae2d75adac061b86ea1778c7f03148
faith-fwzzz <muhammad.fawwaz003@binus.ac.id>
2026-06-22 22:53:24 +0700

    Initial Commit

---
+++ TheMovie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheMovie
+//
+//  Created by FWZ on 22/06/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
